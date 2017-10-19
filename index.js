edfd1d8cd6753461227b6a8be3690f61ae768f3a
Francis <francis.menkes@gmail.com>
2017-10-19 15:19:55 -0300

    CORS header

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ var gatewayURI = process.env.GATEWAY_URI || 'https://jsonplaceholder.typicode.co
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   res.render('index', { gatewayURI: gatewayURI });
 });
